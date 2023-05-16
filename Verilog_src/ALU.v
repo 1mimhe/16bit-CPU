ec53e4502829f47d3a438d86433db80e43d46569
MimHe <mimhe1381@gmail.com>
2023-05-17 01:09:29 +0330

    Implement ControlUnit Verilog code and Update ALU..v

--- Verilog_src/ALU.v
+++ Verilog_src/ALU.v
@@ -1,27 +1,27 @@
 module ALU(
 	input [15:0]A,
 	input [15:0]B,
-	input [2:0]opcode,
-	output reg [15:0]result,
+	input [2:0]Opcode,
+	output reg [15:0]Result,
 	output reg ZERO
 	);
 	integer i;
 
 always @(*)
 begin
-	case(opcode)
-	3'b000 : result = A + B;
-	3'b001 : result = A - B;
-	3'b010 : result = A * B;
-	3'b011 : result = A / B;
-	3'b100 : result = A | B;
-	3'b101 : result = ~(A | B);
-	3'b110 : result = ~(A & B);
+	case(Opcode)
+	3'b000 : Result = A + B;
+	3'b001 : Result = A - B;
+	3'b010 : Result = A * B;
+	3'b011 : Result = A / B;
+	3'b100 : Result = A | B;
+	3'b101 : Result = ~(A | B);
+	3'b110 : Result = ~(A & B);
 	endcase	
 	// assign ZERO
 	for(i=0; i<16; i=i+1)
 	begin
-	ZERO = ZERO | result[i];
+	ZERO = ZERO | Result[i];
 	end
 	ZERO = ~ZERO;
 end
