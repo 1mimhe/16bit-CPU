e26167892e574b99c2c03c1350f2685244fcd88c
MimHe <mimhe1381@gmail.com>
2023-06-09 13:52:52 +0330

    'RegisterFile.v' Implemented

---
+++ Verilog_src/RegisterFile.v
@@ -0,0 +1,23 @@
+module RegisterFile(
+		input clk,
+		input regwrite,
+		input[3:0] regread1,
+		input[3:0] regread2,
+		input[3:0] regdst,
+		input[15:0] writedata,
+		output wire[15:0] readdata1,
+		output wire[15:0] readdata2
+		);	
+					 
+	reg[15:0] R[0:15];
+	
+	assign readdata1 = R[regread1];
+	assign readdata2 = R[regread2];
+
+	always @(posedge clk)
+	begin
+		if(regwrite) R[regdst] = writedata;
+	end
+	
+endmodule
+
