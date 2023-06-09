b701702604c50993ff9a6a984ef768882b344044
MimHe <mimhe1381@gmail.com>
2023-06-09 14:30:27 +0330

    'Memory.v' Implemented

---
+++ Verilog_src/Memory.v
@@ -0,0 +1,22 @@
+module Memory(
+		input clk,
+		input memwrite,
+		input memread,
+		input[15:0] address,
+		input[15:0] writedata,
+		output reg[15:0] readdata
+		);
+	
+	reg[15:0] memory[0:65535];
+	
+	always @(posedge clk)
+	begin
+		if(memwrite) memory[address] <= writedata;
+	end
+	
+	always @(negedge clk)
+	begin
+		if(memread) readdata = memory[address];
+	end
+	
+endmodule
