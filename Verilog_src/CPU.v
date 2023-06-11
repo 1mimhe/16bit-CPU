60beda5a452ef63a9d849c0e873720276378e526
MimHe <mimhe1381@gmail.com>
2023-06-11 20:10:20 +0330

    'CPU.v' Implemented^-^
    happy

--- Verilog_src/CPU.v
+++ Verilog_src/CPU.v
@@ -1,46 +1,74 @@
 module CPU(
-	input clk,
-	input clear,
-	output[15:0] ALURes, pc
+	input clk
     );
-	 
-	 wire [15:0] WriteData;
-	 wire [15:0] ALURes;
-	 wire [15:0] MemRes;
-	 
-	 wire [15:0]ins;
-	 wire zero;
-	 wire branch;
-	 wire [19:0]imm;
-	 
-	 InstructionMemory ins_mem(clk, zero, branch, imm, ins);
-	 
-	 wire lw;
-	 wire sw;
-	 wire rtype;
-	 Control_Unit cu(ins[19:16], branch, lw, sw, rtype);
-	 
-	 wire [3:0] out;
-	 ControlUnit acu(ins[19:16], rtype, branch, out);
-	 
-	 wire [3:0] Dest;
-	 wire RW;
-	 
-	 mid1(rtype, lw, ins, Dest, ALURes, MemRes, WBData, RW);
-	 
-	 wire [3:0] Reg1;
-	 wire [3:0] Reg2;
-	 wire clear_reg;
-	 wire [19:0] out_reg1;
-	 wire [19:0] out_reg2;
-	 
-	 RegisterFile(clk, RW, Dest, WBData, clear_reg, Reg1, Reg2, out_reg1, out_reg2);
+	reg[15:0] pc;
+	wire RegWrite; // Control Unit Signal
+	wire MemWrite;
+	wire MemRead;
+	wire MemToReg;
+	wire ALUsrc;
+	wire Branch;
+	wire RegDst;
+	wire[2:0] ALUctr;
+	wire zero;
+	wire[15:0] instruction;
+	wire ExtOp;
+	reg[15:0] ext_imm;
+	wire[15:0] readdata1;
+	wire[15:0] readdata2;
+	wire[15:0] ALUResult;
+	reg[3:0] regwrite; // Register Destination
+	wire[15:0] readdata; // From Memory
+	reg [15:0] writedata;
+	reg[15:0] ALUinput2;
 
-	 ALU(Reg1, Reg2, out, ALURes, zero);
-	 
-	 wire writeif;
-	 wire readif;
-	 wire clear_mem;
-	 
-	 Memory(clk, ALURes, out_reg2, writeif, readif, clear_mem, MemRes);
+	InstructionMemory ins_mem(pc, instruction);
+
+	ControlUnit control_unit(instruction[15:12], RegDst, ALUsrc, MemToReg, RegWrite, MemWrite, MemRead, Branch, ExtOp, ALUctr);
+
+	always @(*)
+	begin
+		if (ExtOp)
+			ext_imm <= instruction[3:0] <<< 12;
+		else
+			ext_imm <= instruction[3:0] << 12;
+	end
+	
+	always @(posedge clk)
+	begin
+		if (Branch && zero)
+			pc <= (pc + 1) + ext_imm;
+		else
+			pc <= pc + 1;
+	end
+	
+	always @(*)
+	begin
+		if (RegDst)
+			regwrite <= instruction[3:0];
+		else
+			regwrite <= instruction[7:4];
+	end
+	
+	RegisterFile regfile(clk, RegWrite, instruction[11:8], instruction[7:4], regwrite, writedata, readdata1, readdata2);
+	
+	always @(*)
+	begin
+		if (ALUsrc)
+			ALUinput2 <= ext_imm;
+		else
+			ALUinput2 <= readdata2;
+	end
+	
+	ALU alu(readdata1, ALUinput2, ALUctr, ALUResult, zero);
+
+	always @(*)
+	begin
+		if (MemToReg)
+			writedata <= readdata;
+		else
+			writedata <= ALUResult;
+	end
+	
+	Memory memory(clk, MemWrite, MemRead, ALUsrc, readdata2, readdata);
 endmodule
